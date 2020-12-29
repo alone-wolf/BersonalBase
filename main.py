11677b3c1b8cc8b83e198e5195e8885662f22827
alone-wolf <1031629044@qq.com>
2020-12-29 16:35:53 +0800

    update main.py nothing for change

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from PersonalBase import BaseApp
 
 if __name__ == '__main__':
     main_path = os.path.abspath(".")
-    BaseApp("{}/PersonalBase/templates".format(main_path), "{}/PersonalBase/static".format(main_path),
-            host="0.0.0.0", port=5000, debug=False).run()
+    BaseApp("{}/PersonalBase/templates".format(main_path),
+            "{}/PersonalBase/static".format(main_path),
+            host="0.0.0.0",
+            port=5000,
+            debug=False).run()
