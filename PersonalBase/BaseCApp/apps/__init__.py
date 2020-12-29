85616f69bd5bf040d96550323aee22809e40e249
alone-wolf <1031629044@qq.com>
2020-12-29 16:26:18 +0800

    change init_apps function

--- PersonalBase/BaseCApp/apps/__init__.py
+++ PersonalBase/BaseCApp/apps/__init__.py
@@ -4,10 +4,7 @@ from PersonalBase.BaseCApp.apps.Animate import AppSection as AnimateNode
 
 
 def init_apps(server_app, db, admin):
-    apps = (
-        SectionNode(server_app, db, admin),
-        NotifyNode(server_app, db, admin),
+    return \
+        SectionNode(server_app, db, admin), \
+        NotifyNode(server_app, db, admin), \
         AnimateNode(server_app, db, admin),
-    )
-    return apps
-    # return AppSection(server_app, db, admin),
