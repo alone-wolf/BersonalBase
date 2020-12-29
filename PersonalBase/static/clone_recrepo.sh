94bacfe2c641edf951c4857e034f9dccb7a206b4
alone-wolf <1031629044@qq.com>
2020-12-29 16:05:20 +0800

    修改recrepo的克隆脚本使用github

--- PersonalBase/static/clone_recrepo.sh
+++ PersonalBase/static/clone_recrepo.sh
@@ -1,2 +1,2 @@
-git clone /home/wolf/下载/recrepo
-#git clone https://github.com/alone-wolf/recrepo --depth 1
+#git clone /home/wolf/下载/recrepo
+git clone https://github.com/alone-wolf/recrepo --depth 1
