bcadcac79e85efaadd1605ae131151f90265bab6
alone-wolf <1031629044@qq.com>
2020-12-29 16:38:23 +0800

    update nothing for change

--- PersonalBase/__init__.py
+++ PersonalBase/__init__.py
@@ -1,13 +1,1 @@
-import os
-
 from PersonalBase.BaseCApp import Base0App as BaseApp
-
-
-# class BaseCApp(Base1App):
-#     pass
-
-
-# if __name__ == '__main__':
-#     print(os.path.abspath("."))
-#     b = BaseApp("../templates", "../static")
-#     b.run()
